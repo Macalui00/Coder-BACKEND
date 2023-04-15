53935ddc17f12316d2feb8d7ce3c76852160fd14
Lía Macarena Lui <39487444+Macalui00@users.noreply.github.com>
2023-04-15 16:18:09 -0300

    Cierrre del Primer Desafío

--- ProductManager.js
+++ ProductManager.js
@@ -14,6 +14,7 @@ class ProductManager {
                 code,
                 stock
                 });
+                console.log('Product added successfully.')
             } else {
                 console.log('There is another product with the same code.');
             }
@@ -41,11 +42,39 @@ class ProductManager {
     }
 }
 
-// TESTEO DEL ENTREGABLE
+// TESTING
+
+// CREATE OBJECT OF PRODUCT MANAGER
 const productManager = new ProductManager();
+
+console.log('\nProduct Manager Created.');
+console.log('\n--------------------------------------------\n');
+
+// GET ACTUAL PRODUCTS
+console.log('Get Actual Products: ');
 console.log(productManager.getProducts());
+
+console.log('\n--------------------------------------------\n');
+
+console.log('Adding a new product...');
 productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
+
+console.log('\n--------------------------------------------\n');
+
+console.log('Get Actual Products:');
 console.log(productManager.getProducts());
+
+console.log('\n--------------------------------------------\n');
+
+console.log('Adding a another product...');
 productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
+
+console.log('\n--------------------------------------------\n');
+
+console.log('Get Product By ID = 1:');
 console.log(productManager.getProductByID(1));
+
+console.log('\n--------------------------------------------\n');
+
+console.log('Get Product By ID = 3:');
 console.log(productManager.getProductByID(3));
